da52efc6b3af761b164c4ec130c274b84ad6c852
Pikimir22 <egor.nikitin22@yandex.ru>
2022-12-11 16:12:03 +0300

    v.3

--- src/test/java/qa/quru/allure/WebSteps.java
+++ src/test/java/qa/quru/allure/WebSteps.java
@@ -1,29 +1,38 @@
 package qa.quru.allure;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class WebSteps {
     @Step
     public void openMainPage(String value){
-
+        open(value);
     }
 
     @Step
     public void searchRepository(String repo){
-
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repo);
+        $(".header-search-input").submit();
     }
     @Step
     public void openRepo(String repo){
-
+        $(linkText(repo)).click();
     }
 
     @Step
     public void openIssue(){
-
+        $("#issues-tab").click();
     }
 
     @Step
     public void checkIssue(String issue){
+        $(withText(issue)).should(Condition.exist);
 
     }
 
